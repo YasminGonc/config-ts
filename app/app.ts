c98d6d91b052a8d8c3b1506bdf578912f4063684
Yasmin Gonçalves <yasmin.goncalves97@hotmail.com>
2022-08-27 10:59:00 -0300

    tsc config

--- app/app.ts
+++ app/app.ts
@@ -1,9 +1,1 @@
-import { NegociacaoController } from "./controllers/negociacao-controller.js";
 
-const controller = new NegociacaoController();
-const form = document.querySelector('.form');
-
-form.addEventListener('submit', (event: Event) => {
-    event.preventDefault();
-    controller.adiciona();
-});
